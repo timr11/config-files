efcb96a44adb97a149fffc3eb23d22e05d8a9be5
timromanski <tim@airmatrix.ca>
2020-05-08 17:53:31 -0400

    Update setup_vim and remove syntastic
    setup_vim.sh was previously copying the whole .vim directory, now it just
    creates a symlink from $HOME/.vim to the git version.

--- linux-files/scripts/setup/setup_vim.sh
+++ linux-files/scripts/setup/setup_vim.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-source $LINUX_SETUP_SCRIPTS_DIR/setup_script.sh
+source "$LINUX_SETUP_SCRIPTS_DIR/setup_script.sh"
 
 ### START OF SCRIPT ###
-if [ ! -d $CONFIG_FILES/static/vim ]; then
+if [ ! -d "$CONFIG_FILES/static/vim" ]; then
   >&2 echo "No vim folder in your ${CONFIG_FILES}/static directory!"
   exit 1
 fi
-sudo cp $CONFIG_FILES/static/vim/.vimrc $HOME
+cp "$CONFIG_FILES/static/vim/.vimrc" "$HOME"
+if [ ! -L "$HOME/.vim" ]; then
+  ln -s "$CONFIG_FILES/static/vim/.vim" "$HOME/.vim"
+fi
 
