150a59939be3134ab85fed837438dfdf86b1d4c7
timr11 <timromanski@gmail.com>
2020-08-02 10:42:03 -0400

    Add option for eDP-0 to setup_display.sh

--- linux-files/scripts/setup/setup_display.sh
+++ linux-files/scripts/setup/setup_display.sh
@@ -6,6 +6,8 @@ elif xrandr | grep 'HDMI-1 connected'; then
   xrandr --output VIRTUAL-1 --off --output eDP-1 --off --output DP-1 --off --output HDMI-2 --off --output HDMI-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-2 --off
 elif xrandr | grep 'HDMI-1-1 connected'; then
   xrandr --output DP-0 --off --output DP-1 --off --output eDP-1-1 --off --output DP-1-1 --off --output HDMI-1-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1-2 --off --output HDMI-1-2 --off
+elif xrandr | grep 'DP-0 connected'; then
+  xrandr --output DP-0 --primary --mode 5120x1440 --pos 0x0 --rotate normal --output DP-1 --off --output eDP-1-1 --off --output DP-1-1 --off --output HDMI-1-1 --off --output DP-1-2 --off --output HDMI-1-2 --off
 elif xrandr | grep 'eDP1 connected'; then
   xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off
 elif xrandr | grep 'eDP-1 connected'; then
